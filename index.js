ea507c1a7e7c62b80b536acdb725eae15a153fed
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-03-09 18:06:47 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Follow along with the examples here
+function doNothing() {
+    
+}
+function sayHello(){
+    console.log("Hello!")
+}
+sayHello()
+function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+  }
+  function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+  }
+  function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+  }
+  sayHelloToIsabel();
+  sayHelloToSofia();
+  sayHelloToBrendan();
+
+pr = console.log
+function sayHelloTo(firstName) {
+    pr(`Hello, ${firstName}!`);
+}
+sayHelloTo('paul');
+sayHelloTo('nadola');
+sayHelloTo('oyinga');
+/*sayHelloTo(theGoat);
+sayHelloTo(inPractice);
+sayHelloTo(moringa);
+sayHelloTo(cate);
+sayHelloTo(messi);
+sayHelloTo(rashford);
+sayHelloTo(ronaldo);*/
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+say('Niaje', 'Fala')
+ 
+function add(x, y) {
+    return x + y;
+}
+pr(add(56, 56))
+
+
+
+
